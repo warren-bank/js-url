882fc91c5a4f6305129e2e35d0bca67b4993b4b0
Warren R Bank <warren.r.bank@gmail.com>
2023-03-28 17:33:39 -0700

    update tests: 03-various
    * the methodology used to validate instances of the URL class is new
      - uses the native implementation of the URL class to compare fields
    
    * an interesting observation is that several of the test cases fail,
      but as a result of the native URL class behaving in unexpected ways
      - new (require('url').URL)('//localhost')
          => ERR_INVALID_URL
      - new (require('url').URL)('http://localhost').pathname
          => '/'

--- tests/es6-node/03-various/index.js
+++ tests/es6-node/03-various/index.js
@@ -1,33 +1,45 @@
-const {URL}  = require('../../../es6-node/jsURL')
-const assert = require('assert/strict')
+const {URL: URL_real} = require('url')
+const {URL: URL_test} = require('../../../es6-node/jsURL')
+const assert          = require('assert/strict')
 
 let errors_counter = 0
 
-const test = (url_1, base, expected) => {
+const assert_equality_keys = ['protocol', 'username', 'password', 'hostname', 'port', 'pathname', 'search', 'hash', 'host', 'origin', 'search']
+
+const assert_equality = (a, b) => {
+  for (let key of assert_equality_keys) {
+    try {
+      assert.equal(a[key], b[key])
+    }
+    catch(e) {
+      console.log(`FAIL: ${JSON.stringify({url: a.href, field: key, expected: a[key], asserted: b[key]}, null, 2)}`)
+      errors_counter++
+    }
+  }
+}
+
+const test = (url, base) => {
+  let url_real, url_test
+
   try {
-    const url_2 = URL.resolve(url_1, base)
-
-    const URL_1 = new URL(url_1, base)
-    const URL_2 = new URL(url_2)
-
-    assert.equal(url_2, URL_1.href)
-    assert.equal(url_2, URL_2.href)
-
-    if (!base)
-      assert.equal(url_1, url_2)
-    else if (url_1.indexOf('://') === -1)
-      assert.equal(
-        url_2.lastIndexOf(url_1),
-        (url_2.length - url_1.length)
-      )
-
-    if (expected)
-      assert.equal(expected, URL_1.href)
+    url_real = new URL_real(url, base)
   }
   catch(e) {
-    console.log(`FAIL: ${JSON.stringify({url: url_1, base}, null, 2)}`)
+    console.log(`FAIL: ${JSON.stringify({url, base, error: 'unable to parse using real URL class'}, null, 2)}`)
     errors_counter++
+    return
   }
+
+  try {
+    url_test = new URL_test(url, base)
+  }
+  catch(e) {
+    console.log(`FAIL: ${JSON.stringify({url, base, error: 'unable to parse using test URL class'}, null, 2)}`)
+    errors_counter++
+    return
+  }
+
+  assert_equality(url_real, url_test)
 }
 
 test('//localhost')
@@ -50,40 +62,10 @@ test('http://myusername:mypassword@www.example.com:8080/path/to/myfile?a=1&b=2&c
 {
   const base = 'http://myusername:mypassword@www.example.com:8080/path/to/myfile?a=1&b=2&c[]=3&c[]=4#myhash'
 
-  const test_rel = (url, expected) => {
-    test(url, base, expected)
-  }
+  const test_rel = (url) => test(url, base)
 
-  test_rel('')
-  test_rel('//www.example2.com/')
-  test_rel('/')
-  test_rel('/path2/to/myfile2')
-  test_rel('myfile2')
-  test_rel('myfile2?x=1&y=2&z[]=3&z[]=4')
-  test_rel('myfile2?x=1&y=2&z[]=3&z[]=4#hash2')
-  test_rel('/path2/to/myfile2?x=1&y=2&z[]=3&z[]=4#hash2')
-  test_rel('//www.example2.com/path2/to/myfile2?x=1&y=2&z[]=3&z[]=4#hash2')
-  test_rel('https://www.example2.com/path2/to/myfile2?x=1&y=2&z[]=3&z[]=4#hash2')
-}
-
-{
-  const base = 'http://myusername:mypassword@www.example.com:8080/path/to/myfile?a=1&b=2&c[]=3&c[]=4#myhash'
-
-  const get_baseURL = () => new URL(base)
-
-  const test_rel = (url, expected) => {
-    test(url, base, expected)
-  }
-
-  let val, expectedURL
-
-  val = '?x=1&y=2&z[]=3&z[]=4'
-  expectedURL = get_baseURL(); expectedURL.hash = ''; expectedURL.search = val;
-  test_rel(val, expectedURL.href)
-
-  val = '#hash2'
-  expectedURL = get_baseURL(); expectedURL.hash = val;
-  test_rel(val, expectedURL.href)
+  test_rel('?x=1&y=2&z[]=3&z[]=4')
+  test_rel('#hash2')
 }
 
 if (!errors_counter)
